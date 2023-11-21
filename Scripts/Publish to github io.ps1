dcfda0d02d869b154b37f75aa44276a0fb497009
Matthieu Mourisson <mareek@gmail.com>
2023-11-21 19:49:30 +0100

    Improved github.Io update script

--- Scripts/Publish to github io.ps1
+++ Scripts/Publish to github io.ps1
@@ -6,17 +6,19 @@ if (-not $githubIoRootDir) {
 }
 
 $githubIoMeteoDir = Join-Path $githubIoRootDir "meteo" "data"
-."$PSScriptRoot\Download meteo data.ps1" -finalDir $githubIoMeteoDir
 
-# Update GitHub.io repository
 $previousLocation = Get-Location
 Set-Location -Path $githubIoRootDir
 $currentDate = (Get-Date).ToString("yyyy-MM-dd")
 if ((git log --pretty=oneline --max-count=10 | Select-String "\[Auto\] Updated meteo data \($currentDate\)").Count -eq 0) {
+    ."$PSScriptRoot\Download meteo data.ps1" -finalDir $githubIoMeteoDir
+
+    # Update GitHub.io repository
     Write-Host "Update github.io site"
     git commit --all -m "[Auto] Updated meteo data ($currentDate)"
     git push
-} else {
+}
+else {
     Write-Host "github.io site already up to date"
 }
 
